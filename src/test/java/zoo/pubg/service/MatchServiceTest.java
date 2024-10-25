5ce0f0285b71c8c65a196d6972a0faf214d05524
1000zoo <cjswldn99@gmail.com>
2024-10-25 22:15:38 +0900

    Feat: MatchService

--- src/test/java/zoo/pubg/service/MatchServiceTest.java
+++ src/test/java/zoo/pubg/service/MatchServiceTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,8 +19,7 @@ class MatchServiceTest {
 
     @Test
     @DisplayName("player 기본 API 호출 테스트")
-    @Commit
-    void fetchPlayerStats() throws JsonProcessingException {
+    void fetchMatchHistoryTest() throws JsonProcessingException {
         String shards = "kakao";
         String matchId = "b79c05af-bfb3-4744-b9d2-9ccdbe4c0746";
 
