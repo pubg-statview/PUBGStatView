10625ce291c09c0587842b7c293712df0a557b66
1000zoo <cjswldn99@gmail.com>
2024-10-25 21:47:17 +0900

    Feat: 다수의 Player 조회 및 추가 로직 구현

--- src/test/java/zoo/pubg/service/PlayerServiceTest.java
+++ src/test/java/zoo/pubg/service/PlayerServiceTest.java
@@ -1,12 +1,14 @@
 package zoo.pubg.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,5 +32,25 @@ class PlayerServiceTest {
         String s = playerService.fetchPlayer(shards, name);
         System.out.println(s);
 
+    }
+
+    @Test
+    @DisplayName("여러명의 player 동시 조회 테스트")
+    @Commit
+    void fetchPlayersTest() throws JsonProcessingException {
+        // given
+        List<String> ids = List.of("account.cf2acac6cb9741fba202f293aeaab56d",
+                "account.0af3e703b89040d08c1eda37718ca4ac",
+                "account.2e66f4587ca7483fba08dc7eeea5c5cf",
+                "account.a028088f9ff841bba04f27bf45bc6250",
+                "account.a164c65de7bd46f3a0101d4b8ad4a626",
+                "account.816d091773b940de84eefdbc2dc0e22a",
+                "account.57475652769448ccaa426450a14b2643",
+                "account.54f60cff5a04416a9d3a006c3fe88b3b",
+                "account.3df7ccda72e44db19684387d0f7c4aac"
+        );
+        String shards = "kakao";
+
+        playerService.fetchPlayers(shards, ids);
     }
 }
