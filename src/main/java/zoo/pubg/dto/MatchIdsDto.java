a8558c2a0b6991a9fbdad65b652451e98792973a
1000zoo <cjswldn99@gmail.com>
2024-10-31 13:32:27 +0900

    Refactor: MatchIdsDto record로 변경

--- src/main/java/zoo/pubg/dto/MatchIdsDto.java
+++ src/main/java/zoo/pubg/dto/MatchIdsDto.java
@@ -1,24 +1,9 @@
 package zoo.pubg.dto;
 
 import java.util.List;
-import lombok.Getter;
 import zoo.pubg.constant.Shards;
 import zoo.pubg.vo.MatchId;
 
-@Getter
-public class MatchIdsDto {
+public record MatchIdsDto(Shards shards, List<MatchId> matchIds) {
 
-    private final Shards shards;
-    private final List<MatchId> matchIds;
-
-    public MatchIdsDto(Shards shards, List<MatchId> matchIds) {
-        this.shards = shards;
-        this.matchIds = matchIds;
-    }
-
-    public static MatchIdsDto from(Shards shards, List<String> id) {
-        return new MatchIdsDto(
-                shards, id.stream().map(MatchId::new).toList()
-        );
-    }
 }
