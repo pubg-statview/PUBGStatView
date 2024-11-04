3892c6c9cf6fcd45099bcc363b8bda1c25615205
1000zoo <cjswldn99@gmail.com>
2024-11-04 19:27:08 +0900

    Feat: PlayerMatchResult 페이징 기능 구현

---
+++ src/main/java/zoo/pubg/controller/MatchSearchController.java
@@ -0,0 +1,13 @@
+package zoo.pubg.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import zoo.pubg.service.MatchService;
+
+@RestController
+@RequestMapping("/api/search")
+public class MatchSearchController {
+
+
+}
--- src/main/java/zoo/pubg/repository/PlayerMatchRepository.java
+++ src/main/java/zoo/pubg/repository/PlayerMatchRepository.java
@@ -2,8 +2,15 @@ package zoo.pubg.repository;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 import zoo.pubg.domain.PlayerMatchResult;
+import zoo.pubg.domain.Player;
+
+import java.util.List;
 
 @Repository
 public class PlayerMatchRepository {
@@ -13,5 +20,23 @@ public class PlayerMatchRepository {
 
     public void save(PlayerMatchResult playerMatchResult) {
         em.persist(playerMatchResult);
+    }
+
+    public Page<PlayerMatchResult> findAllWithPagination(Player player, int page, int size) {
+        List<PlayerMatchResult> results = em.createQuery("SELECT pm FROM PlayerMatchResult pm WHERE pm.player = :player", PlayerMatchResult.class)
+                .setParameter("player", player)
+                .setFirstResult(page * size)
+                .setMaxResults(size)
+                .getResultList();
+        long totalResults = countPlayerMatchResultByPlayer(player);
+        Pageable pageable = PageRequest.of(page, size);
+
+        return new PageImpl<>(results, pageable, totalResults);
+    }
+
+    private long countPlayerMatchResultByPlayer(Player player) {
+        return em.createQuery("SELECT COUNT(pm) FROM PlayerMatchResult pm WHERE pm.player = :player", Long.class)
+                .setParameter("player", player)
+                .getSingleResult();
     }
 }
