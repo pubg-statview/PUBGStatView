215ed930df56fb24592d6f69b51eed1c20457aeb
1000zoo <cjswldn99@gmail.com>
2024-11-08 12:12:32 +0900

    Feat: 다른 종류의 게임모드 추가

--- src/main/java/zoo/pubg/service/parser/deserialization/rank/RankedGameModeStats.java
+++ src/main/java/zoo/pubg/service/parser/deserialization/rank/RankedGameModeStats.java
@@ -1,6 +1,14 @@
 package zoo.pubg.service.parser.deserialization.rank;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record RankedGameModeStats(
-        RankDataDetails squad
+        RankDataDetails squad,
+        @JsonProperty("squad-fpp") RankDataDetails squadFpp,
+        RankDataDetails duo,
+        @JsonProperty("duo-fpp") RankDataDetails duoFpp,
+        RankDataDetails solo,
+        @JsonProperty("solo-fpp") RankDataDetails soloFpp
 ) {
+
 }
