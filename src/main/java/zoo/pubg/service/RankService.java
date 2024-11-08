7ffeec75d5b3d063d588ea3dd05b0b51e8b9ee3d
1000zoo <cjswldn99@gmail.com>
2024-11-09 00:38:09 +0900

    Feat: Rank API 호출 후 저장하는 기능 구현

--- src/main/java/zoo/pubg/service/RankService.java
+++ src/main/java/zoo/pubg/service/RankService.java
@@ -1,15 +1,18 @@
 package zoo.pubg.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import zoo.pubg.constant.Shards;
 import zoo.pubg.domain.Player;
+import zoo.pubg.domain.rank.Rank;
 import zoo.pubg.domain.rank.Season;
 import zoo.pubg.repository.RankRepository;
 import zoo.pubg.service.api.PubgBasicApi;
+import zoo.pubg.service.dto.RankedDetailsDto;
 import zoo.pubg.service.parser.RankApiParser;
 import zoo.pubg.service.parser.deserialization.rank.RankDeserializer;
 
@@ -29,6 +32,16 @@ public class RankService {
         String responseString = pubgBasicApi.fetchRank(shards.getShardName(), player.getPlayerId().getPlayerId(),
                 season.getSeasonId().getId());
         RankDeserializer rankDeserializer = parser.parse(responseString);
-        
+        List<RankedDetailsDto> rankedDetailsDtos = rankDeserializer.getAllDetails();
+
+        for (RankedDetailsDto rankedDetailsDto : rankedDetailsDtos) {
+            Rank rank = Rank.builder()
+                    .season(season)
+                    .player(player)
+                    .gameMode(rankedDetailsDto.type())
+                    .rankedDetails(rankedDetailsDto.details())
+                    .build();
+            rankRepository.save(rank);
+        }
     }
 }
