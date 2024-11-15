792752d5825b6b44f1a9bd41f2ace9a9a5486d67
1000zoo <cjswldn99@gmail.com>
2024-11-15 17:25:46 +0900

    Feat: 여러 list 기능 추가

--- src/main/java/zoo/pubg/vo/PlayerNames.java
+++ src/main/java/zoo/pubg/vo/PlayerNames.java
@@ -1,5 +1,6 @@
 package zoo.pubg.vo;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public record PlayerNames(List<PlayerName> playerNames) {
@@ -8,5 +9,31 @@ public record PlayerNames(List<PlayerName> playerNames) {
         return new PlayerNames(
                 playerNames.stream().map(PlayerName::new).toList()
         );
+    }
+
+    public static PlayerNames emptyList() {
+        return new PlayerNames(
+                new ArrayList<>()
+        );
+    }
+
+    public String joinToString() {
+        return String.join(",", playerNames.stream().map(PlayerName::getName).toList());
+    }
+
+    public void add(PlayerName playerName) {
+        playerNames.add(playerName);
+    }
+
+    public boolean isEmpty() {
+        return playerNames.isEmpty();
+    }
+
+    public boolean contains(PlayerName playerName) {
+        return playerNames.contains(playerName);
+    }
+
+    public int size() {
+        return playerNames.size();
     }
 }
