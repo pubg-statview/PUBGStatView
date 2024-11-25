26991bb464babcb5aa75da10515ca9eb092330b7
1000zoo <cjswldn99@gmail.com>
2024-11-26 04:21:28 +0900

    Feat: PlayerMatchResult 리스트로부터 객체 생성 스태틱 메서드 구현

--- src/main/java/zoo/pubg/vo/SquadMatchStatisticalData.java
+++ src/main/java/zoo/pubg/vo/SquadMatchStatisticalData.java
@@ -1,6 +1,8 @@
 package zoo.pubg.vo;
 
 import jakarta.persistence.Embeddable;
+import java.util.List;
+import zoo.pubg.domain.PlayerMatchResult;
 
 @Embeddable
 public record SquadMatchStatisticalData(
@@ -9,4 +11,19 @@ public record SquadMatchStatisticalData(
         Integer totalKills,
         Integer winPlace
 ) {
+    public static SquadMatchStatisticalData from(List<PlayerMatchResult> results) {
+        if (results.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        int totalDbno = 0, totalKills = 0, winPlace = results.get(0).getWinPlace();
+        float totalDamageDealt = 0;
+        for (PlayerMatchResult result : results) {
+            totalDbno += result.getDbno();
+            totalKills += result.getKills();
+            totalDamageDealt += result.getDamageDealt();
+        }
+        return new SquadMatchStatisticalData(
+                totalDbno, totalDamageDealt, totalKills, winPlace
+        );
+    }
 }
